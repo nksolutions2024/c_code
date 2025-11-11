2d9ba60667ee444c915dbf8ebffecd84341343a3
nov55 <sandesh4080@gmail.com>
2025-11-11 14:19:25 +0530

    segmentation fault

--- sscratch/sll.c
+++ sscratch/sll.c
@@ -14,7 +14,7 @@ void insert();
 int main(){
 
 	create_2nodes();
-	display();
+	//display();
 
 }
 
@@ -24,17 +24,18 @@ void create_2nodes(){
 	k1->data = ele;
 	k1->next = NULL;
 	
-	//struct node *k2;
-	int ele = 2;
+/*	//struct node *k2;
+	//int ele = 2;
+	ele = 2;
 	k2->data = ele;
 	k2->next = NULL;
-
+*/
 	//node_1 next
-	k1->next = k2;
+	//k1->next = k2;
 }
 
 void display(){
-	printf("-->[%d]", k1->data);
-	printf("-->[%d]", k2->data);
+	//printf("-->[%d]", k1->data);
+	//printf("-->[%d]", k2->data);
 
 }
